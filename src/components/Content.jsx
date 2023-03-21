cdcb9f0c5270a6e8a9e0b2ddf05a8314c6a88f4d
midCenturyDude7 <mgriffes@gmail.com>
2023-03-21 10:06:15 -0700

    Working with onClick events in components

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,9 +2,24 @@
 import React from "react";
 
 function Content({ handleNameChange }) {
+  function handleClick() {
+    console.log("You clicked it!");
+  }
+
+  function handleClick2(name) {
+    console.log(`${name} was clicked`);
+  }
+
+  function handleClick3(e) {
+    console.log(e);
+  }
+
   return (
     <main>
-      <p>Hello {handleNameChange()}!</p>
+      <p onDoubleClick={handleClick}>Hello {handleNameChange()}!</p>
+      <button onClick={handleClick}>Click it!</button>
+      <button onClick={() => handleClick2("Matt")}>Click it!</button>
+      <button onClick={(e) => handleClick3(e)}>Click it!</button>
     </main>
   );
 }
